2619b8df56feb14cab0a506f573af4cdab647d7a
Zack Philipps <hello@zackphilipps.com>
2019-04-19 13:49:39 -0400

    Add zsh plugins to install script

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/bobthecow/git-flow-completion ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-flow-completion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 npm install -g \
   spaceship-prompt \
   eslint \
