3286595059ecbae1a88fafbf291e48eea11a802b
Zack Philipps <hello@zackphilipps.com>
2022-07-26 14:47:55 -0400

    install node via homebrew

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+brew install node
+
 npm install -g \
   eslint \
   stylelint \
